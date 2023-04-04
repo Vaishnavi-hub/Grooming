000cd92ef6656692af1908f6c816b27046fabba6
vaia <vaishnavi.a@publicissapient.com>
2023-04-04 18:12:55 +0530

    custom ArrayList problem

--- practice/src/main/java/com/vaishu/generics/GenericsArrayList.java
+++ practice/src/main/java/com/vaishu/generics/GenericsArrayList.java
@@ -5,73 +5,84 @@ import java.util.Iterator;
 
 // Write custom Arraylist which accept any object and perform basic operations
 public class GenericsArrayList <T> {
+    private Object[] elements;
+    private int size;
 
-    ArrayList<T> list = new ArrayList<> ();
-
-    public GenericsArrayList(ArrayList<T> list) {
-        this.list = list;
+    public GenericsArrayList() {
+        elements = new Object[10];
+        size = 0;
     }
 
-    public int sizeOfArrayList(){
-        return list.size();
+    public void addElement(T element) {
+        if (size == elements.length) {
+            resize();
+        }
+        elements[size] = element;
+        size++;
     }
 
-    public boolean addElement(T element){
-        return list.add(element);
+    public T getElement(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        return (T) elements[index];
     }
 
-    public void addElementAtIndex(int index, T element){
-        list.add(index, element);
+    public void removeElement(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        for (int i = index; i < size - 1; i++) {
+            elements[i] = elements[i + 1];
+        }
+        size--;
     }
 
-    public T getElement(int index){
-        return list.get(index);
+    public int getSize() {
+        return size;
     }
 
-    public T removeElement(int index){
-        T removedElement = list.remove(index);
-        return removedElement;
-    }
-
-    public Iterator<T> iteratorArrayList(){
-        return list.iterator();
+    private void resize() {
+        Object[] newElements = new Object[elements.length * 2];
+        for (int i = 0; i < elements.length; i++) {
+            newElements[i] = elements[i];
+        }
+        elements = newElements;
     }
 
     public static void main(String[] args) {
 
-        // create ArrayList<String>
-        GenericsArrayList<String> stringObj = new GenericsArrayList<>(new ArrayList<String>());
-        System.out.println("Inserting elements to ArrayList<String>..........");
+        // create String Array
+        GenericsArrayList<String> stringObj = new GenericsArrayList<>();
+        System.out.println("Inserting elements to String[]..........");
         stringObj.addElement("Hello");
         stringObj.addElement("Welcome");
         stringObj.addElement("Test");
-        System.out.println("Adding element at index 1..........");
-        stringObj.addElementAtIndex(1, "Generics");
-        System.out.println("Size of ArrayList<String>..........");
-        System.out.println(stringObj.sizeOfArrayList());
+        stringObj.addElement("Objects");
         System.out.println("Get element at index 2..........");
         System.out.println(stringObj.getElement(2));
         System.out.println("Removing element at index 2..........");
         stringObj.removeElement(2);
-        System.out.println("Print elements in Arraylist<String>..........");
-        Iterator<String> itr = stringObj.iteratorArrayList();
-        while(itr.hasNext()){
-            System.out.println(itr.next());
+        System.out.println("Print elements in String[]..........");
+        for(int i =0; i< stringObj.getSize() - 1; i++){
+            System.out.println(stringObj.getElement(i));
         }
 
-        // create ArrayList<Integer>
-        GenericsArrayList<Integer> integerObj = new GenericsArrayList<>(new ArrayList<Integer>());
-        System.out.println("\nInserting elements to ArrayList<Integer>..........");
+        // create Integer Array
+        GenericsArrayList<Integer> integerObj = new GenericsArrayList<>();
+        System.out.println("\nInserting elements to Integer[]..........");
         integerObj.addElement(10);
         integerObj.addElement(43);
-        System.out.println("Adding element at index 0..........");
-        integerObj.addElementAtIndex(0, 25);
+        integerObj.addElement(25);
+        integerObj.addElement(88);
+        integerObj.addElement(39);
         System.out.println("Removing element at index 1..........");
         integerObj.removeElement(1);
-        System.out.println("Print elements in Arraylist<Integer>..........");
-        Iterator<Integer> itr2 = integerObj.iteratorArrayList();
-        while(itr2.hasNext()){
-            System.out.println(itr2.next());
+        System.out.println("Size of Integer[]..........");
+        System.out.println(stringObj.getSize());
+        System.out.println("Print elements in Integer[]..........");
+        for(int i =0; i< integerObj.getSize() - 1; i++){
+            System.out.println(integerObj.getElement(i));
         }
     }
 }
